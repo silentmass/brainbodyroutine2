8253ac2522cb4fad31c0f4a7e168cbeda79592db
silentmass <juha.leukkunen@gmail.com>
2024-04-06 22:33:10 +0300

    Create task category works with backend

---
+++ app/create-task-category/page.tsx
@@ -0,0 +1,9 @@
+import CreateTaskCategory from "../ui/create-task-category";
+
+export default function Page() {
+    return (
+        <div className="flex w-full h-screen items-center justify-center">
+            <CreateTaskCategory />
+        </div>
+    );
+}
---
+++ next.config.js
@@ -0,0 +1,10 @@
+// module.exports = {
+//     async rewrites() {
+//         return [
+//             {
+//                 source: "/api/:path*",
+//                 destination: `http://127.0.0.1:8000/:path*`, // Proxy to Backend
+//             },
+//         ]
+//     },
+// }
