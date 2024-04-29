6b997e5868dd18901e9e52ed8d4a258258bcb070
silentmass <juha.leukkunen@gmail.com>
2024-04-29 12:15:06 +0300

    Check taskCategories

--- app/tasks/create/page.tsx
+++ app/tasks/create/page.tsx
@@ -13,7 +13,11 @@ export default async function Page () {
     <div className='flex flex-col w-full max-w-2xl border items-center justify-center p-5'>
       <h2>Create task</h2>
       <Suspense fallback={<p>Loading create task form...</p>}>
-        <CreateTaskForm taskCategories={taskCategories} />
+        {taskCategories ? (
+          <CreateTaskForm taskCategories={taskCategories} />
+        ) : (
+          <>No task categories</>
+        )}
       </Suspense>
     </div>
   )
