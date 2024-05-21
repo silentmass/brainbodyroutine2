456fdc9666c825122b94e2710a7dc68f68e101d7
silentmass <juha.leukkunen@gmail.com>
2024-05-21 10:33:51 +0300

    Main page

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,6 @@
 import { Metadata } from 'next'
+import { FormButton } from './ui/form-components/buttons'
+import Link from 'next/link'
 
 export const metadata: Metadata = {
   title: 'Brain Body Routine App'
@@ -6,8 +8,27 @@ export const metadata: Metadata = {
 
 export default function Home () {
   return (
-    <div className='flex w-full justify-center'>
-      <h2>Brain body routine</h2>
+    <div className='flex flex-col w-full h-full justify-start'>
+      <div className='flex h-1/6 '></div>
+      <div className='flex flex-col gap-20 justify-center items-center'>
+        <h1>Brain-Body Routine</h1>
+        <h2>
+          Action templates and ideas ranging from super easy to extremely
+          challenging to build complete wellness.
+        </h2>
+        <p>
+          The pillars of well-being include Brain, Body, Recovery, Social
+          interaction, Reflection, and Nutrition.
+        </p>
+        <div className='flex gap-6 items-center'>
+          <p>Go to</p>
+          <Link href='/tasks/filter'>
+            <FormButton type={undefined} ariaLabel='Go to Tasks' className=''>
+              Tasks
+            </FormButton>
+          </Link>
+        </div>
+      </div>
     </div>
   )
 }
