9769d371410ac3e881f047fa1c61982ccc6495b2
silentmass <juha.leukkunen@gmail.com>
2024-05-21 10:44:10 +0300

    Main page single quote fix

--- app/page.tsx
+++ app/page.tsx
@@ -11,17 +11,17 @@ export default function Home () {
     <div className='flex flex-col w-full h-full justify-start'>
       <div className='flex h-1/6 '></div>
       <div className='flex flex-col gap-20 justify-center items-center'>
-        <h1>Brain-Body Routine</h1>
+        <h1>{`Brain-Body Routine`}</h1>
         <h2>
-          Action templates and ideas ranging from super easy to extremely
-          challenging to build complete wellness.
+          {`Action templates and ideas ranging from super easy to extremely
+          challenging to build complete wellness.`}
         </h2>
         <p>
-          The pillars of well-being include Brain, Body, Recovery, Social
-          interaction, Reflection, and Nutrition.
+          {`The pillars of well-being include Brain, Body, Recovery, Social
+          interaction, Reflection, and Nutrition.`}
         </p>
         <div className='flex gap-6 items-center'>
-          <p>Let's start with</p>
+          <p>{`Let's start with`}</p>
           <Link href='/tasks/filter'>
             <FormButton type={undefined} ariaLabel='Go to Tasks' className=''>
               Tasks
