ea139fdf834083ca74272a679c2fe5da8a6640f0
silentmass <juha.leukkunen@gmail.com>
2024-08-02 11:13:40 +0300

    Removed unused import

--- app/demos/router-replace-url/page.tsx
+++ app/demos/router-replace-url/page.tsx
@@ -1,5 +1,3 @@
-import RouterReplaceUrlDemo from '@/app/_components/demo-components/RouterReplaceUrlDemo'
-
 export default function Page () {
   return (
     <div className='page h-full items-center'>
